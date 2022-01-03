dce39702bbd8aa4fbd5fdc6dbefd31fb314c01b7
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-02 19:23:44 -0800

    email and phone number added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,6 +2,8 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
+      <a>(805).407.2015</a>
+      <a href="mailto: daniel.diaz.0515@gmail.com">Email Me</a>
     </div>
   );
 };
