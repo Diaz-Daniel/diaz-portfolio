979ac8d342e8a12ea7e685d37f2ed6f259021459
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-02 18:53:14 -0800

    link to resume added

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,6 +2,9 @@ const Resume = () => {
   return (
     <div>
       <h1>Resume</h1>
+      <a href="https://1drv.ms/w/s!AvcTpeNXuDmNhxaRnOgEh-GvNnwU?e=re64jE">
+        Resume
+      </a>
     </div>
   );
 };
