744994c9a7b4e3ce5a118f14830546bad0403fc3
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-02 18:06:31 -0800

    About Me content added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,27 @@
 const About = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+    <section>
+      <section>
+        <h1>About Me</h1>
+      </section>
+      <section>
+        <p>
+          I am currently a Fleet Engineer for a Seattle based company. I worked
+          as a deckhand for two seasons and then transitioned to the Engineering
+          team. While the work can sometimes be dirty, and the issues diverse as
+          they involve different boating systems; I have found process of
+          problem solving to be the most enjoyable part of my job. From quick
+          fixes to complex interconnected systems issues, the process of problem
+          solving remains the same. Cultivating attention to detail and learning
+          to ask better questions has always been an interest of mine.<br></br>
+          Transistioning my career to web development feels seamless, because
+          while the technical problems to be solved are markedly different- the
+          process of finding solutions remains the same. I believe my projects
+          are produced with the same attention to detail that I put into my
+          current work as a Fleet Engineer.
+        </p>
+      </section>
+    </section>
   );
 };
 
