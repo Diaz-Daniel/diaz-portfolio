b1dd81cc004eb56e54ebec7e15ea6d2c23a33429
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-09 10:20:24 -0800

    added bootstrap containers/row/col

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,21 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const Footer = () => {
   return (
-    <section>
-      <footer>
-        <div>
-          <a href="https://github.com/Diaz-Daniel">Github</a>
-          <a href="http://www.linkedin.com/in/daniel-diaz-wd">LinkedIn</a>
-        </div>
-      </footer>
-    </section>
+    <footer>
+      <Container bg="dark" variant="dark " fixed="bottom">
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <a href="https://github.com/Diaz-Daniel">Github</a>
+          </Col>
+          <Col md="auto">
+            <a href="http://www.linkedin.com/in/daniel-diaz-wd">LinkedIn</a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
