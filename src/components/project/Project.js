ea2d0f7c82a4183dad194dcadace45679140b1f4
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-09 10:17:54 -0800

    added styling/ created project boxes/will need to add project  photos and links

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,8 +1,55 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 const Portfolio = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <Container>
+      <div>
+        <h1>Portfolio</h1>
+      </div>
+      <Row>
+        <Col>
+          <a href="#" className="career">
+            <div>
+              <p>Project 1</p>
+            </div>
+          </a>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a href="#" className="rowTwo workTwo">
+            <div>
+              <p>Project 2</p>
+            </div>
+          </a>
+        </Col>
+        <Col>
+          <a href="#" className="rowTwo workThree">
+            <div>
+              <p>Project 3</p>
+            </div>
+          </a>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a href="#" className="rowTwo workTwo">
+            <div>
+              <p>Project 2</p>
+            </div>
+          </a>
+        </Col>
+        <Col>
+          <a href="#" className="rowTwo workThree">
+            <div>
+              <p>Project 3</p>
+            </div>
+          </a>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Portfolio;
