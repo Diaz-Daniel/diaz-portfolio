ebad0bfa5b4c0c10973331aa4a766bf5227153dc
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-08 20:21:34 -0800

    working navbar- will need to adjust image sizing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,10 +3,7 @@ import NavBar from "../NavBar/NavBar";
 const Header = (props) => {
   return (
     <section className="header">
-      <section className="name">Name Here</section>
-      <section className="navbar">
-        <NavBar {...props} />
-      </section>
+      <NavBar {...props} />
     </section>
   );
 };
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,22 +1,36 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
 const NavBar = (props) => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li onClick={() => props.setCurrentPage("About")}>
-            <a href="#about">About Me</a>
-          </li>
-          <li onClick={() => props.setCurrentPage("Project")}>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li onClick={() => props.setCurrentPage("Resume")}>
-            <a href="#resume">Resume</a>
-          </li>
-          <li onClick={() => props.setCurrentPage("Contact")}>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="Image of Daniel Diaz"
+              src="https://lh3.googleusercontent.com/-cN4TziF2dslRa_G_dUIqNY9bRr7oeIXyNSOTntdDTgx3iUsucTXhu7idHMrJOYXGQkPpetY-bMlN_LHUY6FburOVvxfR31Z6Mc7v25x6OrT_Q0GR7nBwK5YnMFl8Asah5LzqIy220YWBBnv9w-JjTWQd-uUAWEFSY-gIyatpMKpzsPP533UtbVnG4NZFfbJ-tmfR_QMKqYtKCfAUk584xEirqXbwGUXqH8API8dtSozjmIaSk6K59ldlR7h5GVyO96Z9xTAEaMdX3Zqa70KgPJ5Uj23LepV-ORlVEof9Z-THr0IFWXlJkd3c_XmZrXH64RxdwXUc478b4-KJojHB6pNW3TByI5-ELADuCAShVuv2RCkgbHurCzHJUkxdsvCeaKR9pSqdezHg8E_-8a7aCfBI9cIjyyoGZJm_Zo-sS98tn_ZHOKaAOSYd45IAO4zuuV6FbdiZ9M-cnJjPpSimWp5Lol3MR7mrXIfga2j9071c1ycWsDq58gU8hbv_rWWBfdQEMnHQ7vuF-Q76yT9uUEI9gXWkdX21z1qiyjHPwc5QZyWr-p82PvIHMTEQTKTkOSC1Az9HIWnRL5rtLFaeIYqAmlCzA7aU9ZsP0qVy_WXK_Eb6rfjp8TxVPAHsw3WR9SnzDI0vZFWasAtuy2kJj7utrXGW2TyWa-eMwfYttQSkA-wzANcb63JeEHkdWA4FzLfuKhLaMbSYr5aKoTOjH7psg=w276-h450-no?authuser=0"
+              width="100"
+              height="100"
+              className="d-inline-block align-top"
+            />{" "}
+            Daniel Diaz
+          </Navbar.Brand>
+
+          <Nav>
+            <Nav.Link onClick={() => props.setCurrentPage("About")}>
+              About
+            </Nav.Link>
+            <Nav.Link onClick={() => props.setCurrentPage("Project")}>
+              Project
+            </Nav.Link>
+            <Nav.Link onClick={() => props.setCurrentPage("Resume")}>
+              Resume
+            </Nav.Link>
+            <Nav.Link onClick={() => props.setCurrentPage("Contact")}>
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
